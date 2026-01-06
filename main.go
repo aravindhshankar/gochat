bfa5b2dccaed450a908a3ac515a066bec0ed872d
aravindhshankar <aravindhswaminathan@gmail.com>
2026-01-06 21:22:26 +0100

    implemented a simple reader of text messages from remote

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
 func main() {
-	fmt.Println("Hello World!")
-
 	ln, err := net.Listen("tcp", "127.0.0.1:8080")
 	if err != nil {
 		fmt.Println(err)
@@ -23,6 +22,26 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	fmt.Println("accepted a connection")
-	fmt.Println(conn.LocalAddr().String())  // My address , i.e. address of the server created in this program by net.Listen()
-	fmt.Println(conn.RemoteAddr().String()) // Address of who is connecting
+
+	readbuf := make([]byte, 100)
+	writebuf := []byte("You've connected to " + conn.LocalAddr().String() + "\n")
+	n, err := conn.Write(writebuf)
+	if err != nil {
+		fmt.Println("Couldn't write to Remote address : ", conn.RemoteAddr().String())
+	}
+	log.Printf("%d bytes written to %s \n", n, conn.RemoteAddr().String())
+
+	for {
+		_, err = conn.Write([]byte("$ ")) // prompt asking the user to input
+		if err != nil {
+			log.Fatal("Connection unstable")
+		}
+		n, err = conn.Read(readbuf)
+		if err != nil {
+			fmt.Println(err)
+		}
+		ostring := string(readbuf[0 : n-1]) // strips the newline at the end
+		log.Printf("%d bytes read from %s \n", n, conn.RemoteAddr().String())
+		fmt.Println(ostring)
+	}
 }
